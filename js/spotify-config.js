21bc4e5f43ae86f5d053d2ca7ebd149b579c7263
Bob_TO <bouvet.clement@gmail.com>
2026-09-25 21:24:46 +0200

    Update spotify-config.js

--- js/spotify-config.js
+++ js/spotify-config.js
@@ -2,4 +2,4 @@
 // https://developer.spotify.com/dashboard — voir README, section Spotify.
 // Laisser vide masque la connexion Spotify : le reste des fonctions
 // musicales (liens, playlists, mur musical, défis) marche sans.
-export const SPOTIFY_CLIENT_ID = "";
+export const SPOTIFY_CLIENT_ID = "3d2302750cc348559d36ace692e2d9ca";
